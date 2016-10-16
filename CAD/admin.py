e05b1c1e21e71ecae9ae6f85d6b1f3c3d20e5252
undeanlili <lizhinewlife@gmail.com>
2016-10-16 20:15:10 +0900

    Add files via upload

--- CAD/admin.py
+++ CAD/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Article
+from .models import Person
 
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'update_time',)
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('full_name',)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Person, PersonAdmin)
 # Register your models here.
--- CAD/views.py
+++ CAD/views.py
@@ -1,6 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
+
+
 
 # Create your views here.
